cd2ff13180a4ca702e890a642038f5fcf5da9720
Varya Stepanova <mail@varya.me>
2015-10-15 17:47:10 +0300

    pure dist

--- dist/react-mapbox.js
+++ dist/react-mapbox.js
@@ -72,16 +72,20 @@
 
 	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var _react = __webpack_require__(2);
 
+	var _react2 = _interopRequireDefault(_react);
+
 	var _reactDom = __webpack_require__(3);
 
-	var MapBox = (function (_Component) {
-	  _inherits(MapBox, _Component);
+	var MapBox = (function (_React$Component) {
+	  _inherits(MapBox, _React$Component);
 
 	  function MapBox() {
 	    _classCallCheck(this, MapBox);
@@ -92,7 +96,7 @@
 	  _createClass(MapBox, [{
 	    key: 'render',
 	    value: function render() {
-	      return React.createElement(
+	      return _react2['default'].createElement(
 	        'h1',
 	        null,
 	        'Test'
@@ -101,7 +105,7 @@
 	  }]);
 
 	  return MapBox;
-	})(_react.Component);
+	})(_react2['default'].Component);
 
 	exports['default'] = MapBox;
 	module.exports = exports['default'];
