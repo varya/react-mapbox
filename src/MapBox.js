f4a451a5e694f04d610551a1700a6c6515edcc15
Varya Stepanova <mail@varya.me>
2015-10-15 17:47:01 +0300

    require React

--- src/MapBox.js
+++ src/MapBox.js
@@ -1,7 +1,7 @@
-import { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-export default class MapBox extends Component {
+export default class MapBox extends React.Component {
   render () {
     return (
         <h1>Test</h1>
